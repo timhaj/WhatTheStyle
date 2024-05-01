d81be38a8d67012bbdf146fc90d690ff5ad65c1a
Tim Hajdinjak <69644152+timhaj@users.noreply.github.com>
2024-05-01 13:20:33 +0200

    Fixed bugs for color guessing game

--- logic.ts
+++ logic.ts
@@ -3,7 +3,7 @@ containerDiv.setAttribute("class", "container");
 let guess: number = Math.floor(Math.random() * 5);
 let available: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 let colors: string[] = generateRandomColors();
-
+let flag: boolean = false;
 let info: any = document.createElement("p");
 info.setAttribute("id", "guessingColor");
 info.innerHTML = colors[guess];
@@ -35,30 +35,35 @@ function check(element: any): void {
     let B: string = parseInt(barve[2], 10).toString(16).toUpperCase().padStart(2, "0");
     let hex: string = "#" + R + G + B;
     if (hex == colors[guess]) {
-        for (let i: number = 0; i < colorDiv.children.length; i++) {
-            let child: any = colorDiv.children[i];
-            if (child !== element) {
-                child.style.backgroundColor = 'transparent';
+        if (!flag) {
+            for (let i: number = 0; i < colorDiv.children.length; i++) {
+                let child: any = colorDiv.children[i];
+                if (child !== element) {
+                    child.style.backgroundColor = 'transparent';
+                }
             }
+            info = document.createElement("p");
+            info.setAttribute("id", "win");
+            info.innerHTML = "You're correct!";
+            containerDiv.replaceChild(info, containerDiv.childNodes[2]);
+            let a: any = document.createElement("a");
+            a.setAttribute("href", "/");
+            info = document.createElement("button");
+            info.setAttribute("id", "btn");
+            info.innerHTML = "NEW GAME";
+            a.appendChild(info);
+            containerDiv.appendChild(a);
+            flag = true;
         }
-        info = document.createElement("p");
-        info.setAttribute("id", "win");
-        info.innerHTML = "You're correct!";
-        containerDiv.replaceChild(info, containerDiv.childNodes[2]);
-        let a: any = document.createElement("a");
-        a.setAttribute("href", "/");
-        info = document.createElement("button");
-        info.setAttribute("id", "btn");
-        info.innerHTML = "NEW GAME";
-        a.appendChild(info);
-        containerDiv.appendChild(a);
     }
     else {
-        element.style.backgroundColor = "transparent";
-        info = document.createElement("p");
-        info.setAttribute("id", "incorrect");
-        info.innerHTML = "Incorrect, that color was: <span style=\"color:" + hex + "\">" + hex + "</span>";
-        containerDiv.replaceChild(info, containerDiv.childNodes[2]);
+        if (!flag) {
+            element.style.backgroundColor = "transparent";
+            info = document.createElement("p");
+            info.setAttribute("id", "incorrect");
+            info.innerHTML = "Incorrect, that color was: <span style=\"color:" + hex + "\">" + hex + "</span>";
+            containerDiv.replaceChild(info, containerDiv.childNodes[2]);
+        }
     }
 }
 
